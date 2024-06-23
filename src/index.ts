e7303f8283f2e8232bba21ed86e9b7e624f92bec
KPolischuk <polischuk.k.v@gmail.com>
2024-06-22 20:56:47 -0600

    BLSPSR-1 project initialization

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import './main.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = 'Hello, Webpack!!!!!!!!!';
+
+  return element;
+}
+
+document.body.appendChild(component());
+
